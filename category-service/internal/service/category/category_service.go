78527d3adc53b7064a0157f0058a6f192dc9a80b
Gaiaz Iusipov <gyusipov@ozon.ru>
2021-11-13 14:46:36 +0300

    Degradation example

--- category-service/internal/service/category/category_service.go
+++ category-service/internal/service/category/category_service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/sync/errgroup"
 
 	internal_errors "github.com/ozonmp/week-5-workshop/category-service/internal/pkg/errors"
 )
@@ -35,5 +37,52 @@ func (s Service) GetCategoryByID(ctx context.Context, id uint64) (*Category, err
 		return nil, ErrNoCategory
 	}
 
+	// Пример с деградацией
+
+	eg, egCtx := errgroup.WithContext(ctx)
+
+	var categoryIcon string
+	eg.Go(func() (egErr error) {
+		categoryIcon, egErr = GetCategoryIcon(egCtx, cat.ID)
+		return errors.Wrap(egErr, "GetCategoryIcon()")
+	})
+
+	var categoryDescription string
+	eg.Go(func() (egErr error) {
+		categoryDescription, egErr = GetCategoryDescription(egCtx, cat.ID)
+		return errors.Wrap(egErr, "GetCategoryIcon()")
+	})
+
+	var categoryRating uint8
+	eg.Go(func() (egErr error) {
+		categoryRating, egErr = GetCategoryRating(egCtx, cat.ID)
+		if egErr != nil {
+			log.Error().Msg("GetCategoryRating()")
+		}
+		return nil
+	})
+
+	err = eg.Wait()
+	if err != nil {
+		return nil, err
+	}
+
+	// Каким-то образом используем значения
+	_, _, _ = categoryIcon, categoryDescription, categoryRating
+
 	return cat, nil
+}
+
+// External call stubs:
+
+func GetCategoryIcon(ctx context.Context, catID uint64) (string, error) {
+	return "", nil
+}
+
+func GetCategoryDescription(ctx context.Context, catID uint64) (string, error) {
+	return "", nil
+}
+
+func GetCategoryRating(ctx context.Context, catID uint64) (uint8, error) {
+	return 0, nil
 }
